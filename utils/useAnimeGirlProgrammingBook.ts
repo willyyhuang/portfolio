c6b003df22e24f6a9c965d90bcf4809d2e68adb4
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2022-05-05 21:26:51 -0400

    fix bug

--- utils/useAnimeGirlProgrammingBook.ts
+++ utils/useAnimeGirlProgrammingBook.ts
@@ -1,21 +1,30 @@
 import {useEffect, useState} from 'react'
 
+type FolderEntry = {
+  folderName: string
+  files: Array<string>
+}
+
 const rawImageUrlPrefix =
   'https://raw.githubusercontent.com/cat-milk/Anime-Girls-Holding-Programming-Books/master/'
 
 const useAnimeGirlProgrammingBook = (animeGirlProgramming: any) => {
-    const [selectedProgrammingLanguage, setSelectedProgrammingLanguage] = useState<any>()
+    const [selectedProgrammingLanguage, setSelectedProgrammingLanguage] = useState<string>()
     const [currentImage, setCurrentImage] = useState<string>()
 
-    const entries = animeGirlProgramming?.repository?.object?.entries.map((folder: any) => {
-      const files = folder.object.entries?.map((file: any) => rawImageUrlPrefix.concat(file.path))
+    const formattedData = animeGirlProgramming?.repository?.object?.entries.map((folder: any) => {
+      const {entries} = folder.object
+      const files = entries ? entries.map((file: {path: string}) => rawImageUrlPrefix.concat(file.path)) : []
       return {
         folderName: folder.name,
         files,
       }
     })
+
+    const entries = formattedData?.filter((folder: FolderEntry) => folder.files.length > 0)
+
     const refreshImage = () => {
-        const directory = entries.find((entry: any) => entry.folderName === selectedProgrammingLanguage)
+        const directory = entries.find((entry: FolderEntry) => entry.folderName === selectedProgrammingLanguage)
         if (directory) {
             const {files} = directory
             setCurrentImage(files[Math.floor(Math.random() * files.length)])
