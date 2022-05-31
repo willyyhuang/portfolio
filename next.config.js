bf38004801b05ba53240e24ec93d0daab32024eb
Wilson Wei Chen Huang <48188884+willyyhuang@users.noreply.github.com>
2022-05-31 00:49:46 -0400

    disable pwa in test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 const withPWA = require('next-pwa')
 
+const prod = process.env.NODE_ENV === 'production'
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
+    disable: !prod,
   },
   reactStrictMode: true,
 })
